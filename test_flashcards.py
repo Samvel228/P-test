7b88b232a1540b5d468f1aea3fc4df222d1ebfeb
Kocharan <kocharya@cs.karelia.ru>
2023-12-01 10:02:04 +0300

    added tests

--- test_flashcards.py
+++ test_flashcards.py
@@ -1,9 +1,9 @@
 import unittest
 from flashcard import Flashcard
 from flashcard_deck import FlashcardDeck
-from unittest.mock import patch, mock_open, MagicMock
+from unittest.mock import patch, mock_open
 import pickle
-import io
+
 
 class TestFlashcardMethods(unittest.TestCase):
 
@@ -35,12 +35,10 @@ class TestFlashcardDecMethods(unittest.TestCase):
         flashcard1 = Flashcard("Term1", "Definition1")
         deck.add_flashcard(flashcard1)
 
-        # Подставляем "правильный" ответ при вызове input
         with patch('builtins.input', return_value="Definition1"):
             with patch('builtins.print') as mock_print:
                 deck.study_flashcards()
 
-        # Проверяем, что было выведено "Правильно"
         mock_print.assert_called_with("Правильно")
 
     def test_study_flashcards_incorrect_answer(self):
@@ -48,54 +46,61 @@ class TestFlashcardDecMethods(unittest.TestCase):
         flashcard1 = Flashcard("Term1", "Definition1")
         deck.add_flashcard(flashcard1)
 
-        # Подставляем "неправильный" ответ, который не совпадает с Definition1
         with patch('builtins.input', return_value="IncorrectAnswer"):
             with patch('builtins.print') as mock_print:
                 deck.study_flashcards()
 
-        # Проверяем, что было выведено "Ошибка! правильное определение: Definition1"
         mock_print.assert_called_with("Ошибка! правельное определение: Definition1")
     
     @patch('builtins.open', new_callable=mock_open)
     @patch('flashcard_deck.pickle.dump')
     def test_save_flashcard_deck(self, mock_pickle_dump, mock_open):
-        # Создаем объект FlashcardDeck и добавляем в него фиктивную карточку
         deck = FlashcardDeck()
-        fake_flashcard = Flashcard("Term", "Definition")
-        deck.flashcards.append(fake_flashcard)
+        flashcard = Flashcard("Term", "Definition")
+        deck.flashcards.append(flashcard)
 
-        # Вызываем функцию сохранения
         deck.save_flashcard_deck("test_deck.pkl")
 
-        # Проверяем, что функция open вызывалась с ожидаемыми аргументами
         mock_open.assert_called_once_with("test_deck.pkl", 'wb')
 
-        # Проверяем, что функция pickle.dump вызывалась с ожидаемыми аргументами
-        mock_pickle_dump.assert_called_once_with([fake_flashcard], mock_open.return_value)
+        mock_pickle_dump.assert_called_once_with([flashcard], mock_open.return_value)
     
     @patch('builtins.open', new_callable=mock_open)
     @patch('flashcard_deck.pickle.load')
     def test_load_flashcard_deck(self, mock_pickle_load, mock_open):
-        # Подготавливаем фиктивные данные для загрузки
-        fake_flashcard = Flashcard("Term", "Definition")
-        fake_flashcards = [fake_flashcard]
+        flashcard = Flashcard("Term", "Definition")
+        flashcards = [flashcard]
 
-        # Мокаем функцию open и load
-        mock_open.return_value.__enter__.return_value.read.return_value = "fake_data"
-        mock_pickle_load.return_value = fake_flashcards
+        mock_open.return_value.__enter__.return_value.read.return_value = "data"
+        mock_pickle_load.return_value = flashcards
 
-        # Создаем объект FlashcardDeck и вызываем функцию загрузки
         deck = FlashcardDeck()
         deck.load_flashcard_deck("test_deck.pkl")
 
-        # Проверяем, что функция open вызывалась с ожидаемыми аргументами
         mock_open.assert_called_once_with("test_deck.pkl", 'rb')
 
-        # Проверяем, что функция pickle.load вызывалась с ожидаемыми аргументами
         mock_pickle_load.assert_called_once_with(mock_open.return_value)
 
-        # Проверяем, что flashcards в объекте FlashcardDeck были обновлены
-        self.assertEqual(deck.flashcards, fake_flashcards)
+        self.assertEqual(deck.flashcards, flashcards)
+
+    def test_load_flashcard_deck_file_not_found(self):
+        deck = FlashcardDeck()
+        
+        with unittest.mock.patch('builtins.open', side_effect=FileNotFoundError):
+            with unittest.mock.patch('builtins.print') as mock_print:
+                deck.load_flashcard_deck("nonexistent_file.pkl")
+
+        mock_print.assert_called_once_with(f"Ошибка: файл 'nonexistent_file.pkl' не найден.")
+
+
+    def test_load_flashcard_deck_unpickling_error(self):
+        deck = FlashcardDeck()
+
+        with unittest.mock.patch('builtins.open', side_effect=pickle.UnpicklingError):
+            with unittest.mock.patch('builtins.print') as mock_print:
+                deck.load_flashcard_deck("corrupted_file.pkl")
+
+        mock_print.assert_called_once_with(f"Ошибка: невозможно загрузить данные из 'corrupted_file.pkl'.")
 
 if __name__ == '__main__':
     unittest.main()
